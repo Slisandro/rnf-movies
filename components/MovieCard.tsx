5874c2961c0d77bd8faf1ab95fcb0586c6d7331a
Slisandro <salvareschilisandro0@gmail.com>
2025-08-11 20:09:15 -0300

    Styles MovieCard and SearchBar

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,13 +5,13 @@ import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
     return (
-        <Link href={`/movies/${id}`}>
-            <TouchableOpacity className="mb-5">
+        <Link href={`/movies/${id}`} asChild>
+            <TouchableOpacity className="w-[30%]">
                 <Image
                     source={{
                         uri: poster_path
                             ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                            : "https://placehold.co/600x400/1a1a1a/ffffff.png"
+                            : "https://placehold.co/600x400/1a1a1a/FFFFFF.png",
                     }}
                     className="w-full h-52 rounded-lg"
                     resizeMode="cover"
@@ -25,8 +25,12 @@ const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie
                 </View>
 
                 <View className="flex-row items-center justify-between gap-x-1">
-                    <Text className="text-xs text-light-300 font-medium mt-1">{release_date?.split("-")[0]}</Text>
-                    <Text className="text-xs text-light-300 font-medium uppercase">{release_date?.split("-")[0]}</Text>
+                    <Text className="text-xs text-light-300 font-medium mt-1">
+                        {release_date?.split("-")[0]}
+                    </Text>
+                    <Text className="text-xs font-medium text-light-300 uppercase">
+                        Movie
+                    </Text>
                 </View>
             </TouchableOpacity>
         </Link>
---
+++ components/SearchBar.tsx
@@ -0,0 +1,32 @@
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
+
+interface SearchBarProps {
+    onPress: () => void;
+    placeholder: string
+}
+
+const SearchBar = ({ onPress, placeholder }: SearchBarProps) => {
+    return (
+        <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+            <Image
+                source={icons.search}
+                className="size-5"
+                resizeMode="contain"
+                tintColor="#ab8bff"
+            />
+
+            <TextInput
+                placeholder={placeholder}
+                className="flex-1 ml-2 text-white"
+                placeholderTextColor="#a8b5db"
+                value=""
+                onChangeText={() => { }}
+                onPress={onPress}
+            />
+        </View>
+    )
+}
+
+export default SearchBar
